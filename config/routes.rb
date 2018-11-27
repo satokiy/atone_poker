6ea5e66d66999abbdf59e3abc4f0fcbc973ab08c
山田 悟生 <s.yamada@MAC0108.local>
2018-11-27 16:07:15 +0900

    backup2

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post '/check' => "home#check"
   put '/check' => "home#check"
 
-  mount PokerApi => '/'
+  mount PokerApi => '/api'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
